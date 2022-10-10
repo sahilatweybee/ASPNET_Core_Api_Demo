51db26eb26270cc7e9d9d17151215b2674b14ed8
Sahil Parsaniya <sahil@webtheuniverse.com>
2022-10-10 18:18:28 +0530

    final commit

--- ASPNET_Core_Api_Demo/Startup.cs
+++ ASPNET_Core_Api_Demo/Startup.cs
@@ -17,13 +17,11 @@ namespace ASPNET_Core_Books_Api_Demo
 {
     public class Startup
     {
-
+        public IConfiguration Configuration { get; }
         public Startup(IConfiguration configuration)
         {
             Configuration = configuration;
         }
-
-        public IConfiguration Configuration { get; }
 
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
